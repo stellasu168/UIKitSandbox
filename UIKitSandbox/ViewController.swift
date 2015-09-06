6aa80042ecf9baf26f9ecb4c4c39d2caf4ac2d2d
Stella <stella@edmodo.com>
2015-09-06 14:01:27 -0700

    first commit

--- UIKitSandbox/ViewController.swift
+++ UIKitSandbox/ViewController.swift
@@ -8,17 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+
+    @IBOutlet weak var textTop: UITextField!
+    @IBOutlet weak var textBottom: UITextField!
+    @IBOutlet weak var buttomToolbar: UIToolbar!
+    @IBOutlet weak var imagePickView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        textTop.textAlignment = .Center
+        
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        // Disable the camera button if the device doesn't have a camera
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+    }
+    
+  
+    // Function that allows the user to pick an image from either the photo library or from their camera
+    @IBAction func pickAnImage(sender: AnyObject) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        if sender.title == "Album"{
+            imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            presentViewController(imagePicker, animated: true, completion: nil)
+        } else{
+            imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+            presentViewController(imagePicker, animated: true, completion: nil)
+        }
+        //self.dismissViewControllerAnimated(true, completion: nil)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Passes the image selected by the user to the imagePickView
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            //imagePickView.contentMode = UIViewContentMode.ScaleAspectFit
+            imagePickView.image = image
+            
+        } else {
+            println("No image was picked")
+        }
+        dismissViewControllerAnimated(true, completion: nil)
     }
+    
+
 
 
 }
